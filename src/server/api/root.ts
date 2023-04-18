5d087622840bd79260dedfa99fa470db7e9d8c1c
maxilie <max@maxilie.com>
2023-04-18 09:30:35 -0400

    Fix routing typo

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { usersRouter } from "./routers/protected";
+import { tasksRouter } from "./routers/protected";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { usersRouter } from "./routers/protected";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  managedUsers: usersRouter,
+  managedUsers: tasksRouter,
 });
 
 // export type definition of API
