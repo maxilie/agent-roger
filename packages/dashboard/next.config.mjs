cf2d102213366e11b9824df1782437508528ee34
maxilie <max@maxilie.com>
2023-05-07 09:07:05 -0400

    Debug vercel build error

--- packages/dashboard/next.config.mjs
+++ packages/dashboard/next.config.mjs
@@ -2,8 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION &&
-  (await import("agent-roger-core/dist/src/env.mjs"));
+!process.env.SKIP_ENV_VALIDATION && (await import("agent-roger-core"));
 
 /** @type {import("next").NextConfig} */
 const config = {
