aa7726b654dd99c171341b003e12005b8351eec3
maxilie <max@maxilie.com>
2023-05-20 07:33:52 -0400

    Make all stage function names unique

--- packages/agent-roger-core/src/stage/task-index-file/index.ts
+++ packages/agent-roger-core/src/stage/task-index-file/index.ts
@@ -5,7 +5,7 @@ import {
 
 export const INDEX_FILE_STAGE_FNS: { [key: string]: StageFunction } = {
   // eslint-disable-next-line @typescript-eslint/require-await
-  splitText: async (helpers: StageFunctionHelpers) => {
+  splitText1: async (helpers: StageFunctionHelpers) => {
     helpers.endStage();
   },
 };
--- packages/agent-roger-core/src/stage/task-modify-file/index.ts
+++ packages/agent-roger-core/src/stage/task-modify-file/index.ts
@@ -5,7 +5,7 @@ import {
 
 export const MODIFY_FILE_STAGE_FNS: { [key: string]: StageFunction } = {
   // eslint-disable-next-line @typescript-eslint/require-await
-  splitText: async (helpers: StageFunctionHelpers) => {
+  splitText2: async (helpers: StageFunctionHelpers) => {
     helpers.endStage();
   },
 };
